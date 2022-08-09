7293d05f92b5a4bfc25db63f3763793c8a6d3edc
Raycris Maldonado <raycris.maldonado@utreee.com>
2022-08-09 17:44:15 -0400

    reparando errores de esLint

--- tienda/src/hooks/useGetProduct.js
+++ tienda/src/hooks/useGetProduct.js
@@ -1,31 +1,22 @@
-// import { useEffect, useState } from "react";
-
-// import axios from "axios";
-
-// const useGetProducts = (API) => {
-//   const [products, setProducts] = useState([]);
-//   const [isLoading, setIsLoading] = useState(true);
-
-//   useEffect(async () => {
-//     const response = await axios(API);
-//     setProducts(response.data);
-//     setIsLoading(false);
-//   }, []);
-
-//   // return  {products, isLoading};
-//   return products;
-// };
-
-// export default useGetProducts;
-
 import { useEffect, useState } from 'react';
+
 import axios from 'axios';
+
 const useGetProducts = (API) => {
   const [products, setProducts] = useState([]);
-  useEffect(async () => {
-    const response = await axios(API);
-    setProducts(response.data);
-  }, []);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios(API);
+      setProducts(response.data);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, [API]);
+
+  // return  {products, isLoading};
   return products;
 };
+
 export default useGetProducts;
--- tienda/src/pages/checkout.js
+++ tienda/src/pages/checkout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 import Image from 'next/image';
 import { Head } from 'next/document';
 
@@ -11,24 +10,24 @@ import styles from '@styles/Checkout.module.scss';
 const Checkout = () => {
   return (
     <>
-    <Head>
-      <title>Checkout</title>
-    </Head>
-    <div className={styles['my-order']}>
-      <div className={styles['my-order-container']}>
-        <h1 className={styles.title}>My order</h1>
-        <div className={styles['my-order-content']}>
-          <div className={styles.order}>
-            <p>
-              <span>04.24.25</span>
-              <span>7 articulos</span>
-            </p>
-            <p>$560.00</p>
-            <Image src={Arrow} alt="arrow" />
+      <Head>
+        <title>Checkout</title>
+      </Head>
+      <div className={styles['my-order']}>
+        <div className={styles['my-order-container']}>
+          <h1 className={styles.title}>My order</h1>
+          <div className={styles['my-order-content']}>
+            <div className={styles.order}>
+              <p>
+                <span>04.24.25</span>
+                <span>7 articulos</span>
+              </p>
+              <p>$560.00</p>
+              <Image src={Arrow} alt="arrow" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
